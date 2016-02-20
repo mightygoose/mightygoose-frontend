dbc8515bde55b91fa6edf24c4c2c1ada4fcf75ee
Alexander Zhukov <al.zhukoff@gmail.com>
2016-02-20 21:25:04 +0100

    post light

--- assets/components/post/post.js
+++ assets/components/post/post.js
@@ -1,9 +1,10 @@
 const BaseComponent = require('lib/base_component');
 const template = require('ejs!./post.html');
+const styles = require('style!css!stylus!./post.styl');
 
 class PostItem extends BaseComponent {
   render(data){
-    this.innerHTML = template({post: data});
+    this.innerHTML = template(data);
   }
   create(){
     console.log('post item created');
