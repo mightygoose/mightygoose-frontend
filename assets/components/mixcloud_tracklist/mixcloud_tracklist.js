a466961a1c34be2ae461660ac55dc2901e6b9e49
Alexander Zhukov <al.zhukoff@gmail.com>
2016-02-24 04:12:37 +0100

    add start time to track

--- assets/components/mixcloud_tracklist/mixcloud_tracklist.js
+++ assets/components/mixcloud_tracklist/mixcloud_tracklist.js
@@ -9,11 +9,29 @@ class MixcloudTracklist extends BaseComponent {
     this.innerHTML = template({
       image: data.cloudcast.pictures.mobile_player_full,
       title: data.cloudcast.title,
+      format_position(seconds){
+        var secs = Math.round(seconds);
+        var hours = Math.floor(secs / (60 * 60));
+
+        var divisor_for_minutes = secs % (60 * 60);
+        var minutes = Math.floor(divisor_for_minutes / 60);
+
+        var divisor_for_seconds = divisor_for_minutes % 60;
+        var seconds = Math.ceil(divisor_for_seconds);
+
+        return [
+          hours < 10 ? `0${hours}` : `${hours}`,
+          minutes < 10 ? `0${minutes}` : `${minutes}`,
+          seconds < 10 ? `0${seconds}` : `${seconds}`,
+        ].join(':');
+      },
       each_artist(tpl){
         return _.map(data.cloudcast.featuring_artist_list, (artist) => tpl(artist)).join("");
       },
-      each_track(tpl){
-        return _.map(data.cloudcast.sections, (track) => tpl(track)).join("");
+      each_track(tpl, fallback_tpl){
+        return _.map(data.cloudcast.sections, (track) => {
+          return (track.artist && track.title) ? tpl(track) : fallback_tpl(track);
+        }).join("")
       }
     });
   }
