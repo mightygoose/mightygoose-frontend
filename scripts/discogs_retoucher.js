ffa9b5669d2838230967e2f58709c717d5ec76f2
Alexander Zhukov <al.zhukoff@gmail.com>
2016-03-11 10:47:03 +0100

    add discogs retoucher script

---
+++ scripts/discogs_retoucher.js
@@ -0,0 +1,29 @@
+var btc = require('bloom-text-compare');
+
+
+var _ = require('lodash');
+
+
+var stream = process.stdin;
+
+var content = "";
+stream.on('data', function(data) {
+  content += data.toString();
+});
+
+stream.on('end', function(data) {
+  var item = JSON.parse(content);
+  //console.log(item);
+  var title1 = item.title.replace(/[^a-zA-Z0-9 ]/ig, '').toLowerCase();
+  var title2 = item.discogs.title.replace(/[^a-zA-Z0-9 ]/ig, '').toLowerCase();
+  var hash1 = btc.hash(title1.split(' '));
+  var hash2 = btc.hash(title2.split(' '));
+
+  var distance = btc.compare(hash1, hash2);
+  item['discogs_title_similarity'] = distance;
+  if(distance > 0.5){
+    item['badges'].push('discogs-title-similarity-good');
+  }
+  console.log(JSON.stringify(item));
+});
+
