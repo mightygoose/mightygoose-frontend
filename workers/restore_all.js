9cdc858ba16e262e56cc41bfc51d4f6f5d94c4de
Alexander Zhukov <al.zhukoff@gmail.com>
2016-06-12 18:50:41 +0200

    restorer next iteration

--- workers/restore_all.js
+++ workers/restore_all.js
@@ -2,6 +2,9 @@ const massive = require("massive");
 const log = require('log-colors');
 const spawn = require('../lib/spawn');
 
+const itunes_restorer = require('../lib/restorers/itunes');
+const s_digital_restorer = require('../lib/restorers/7digital');
+
 const DB_HOST = process.env['DB_HOST'];
 const DB_PORT = process.env['DB_PORT'];
 const DB_USER = process.env['DB_USER'];
@@ -33,7 +36,17 @@ spawn(function*(){
   var result = yield query("select id from items where s_digital is null and itunes is null limit 10");
 
   for(var item of result){
-    console.log(item.id);
+    var item_data = yield query(`select * from items where id = ${item.id}`);
+    var restored_data = yield [
+      yield itunes_restorer(item_data[0]),
+      yield s_digital_restorer(item_data[0])
+    ];
+
+    var itunes_data = restored_data[0];
+    var s_digital_data = restored_data[1];
+    console.log(item_data[0].title);
+    console.log(`${itunes_data[0].artistName} - ${itunes_data[0].collectionName}`);
+    console.log(`${s_digital_data[0].release.artist.name} - ${s_digital_data[0].release.title}`);
   }
 
   process.exit(0);
