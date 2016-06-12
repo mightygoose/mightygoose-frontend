51c690b6b21db7b440530dacd41d1550a30c02a8
Alexander Zhukov <al.zhukoff@gmail.com>
2016-06-12 20:16:17 +0200

    finish restorer

--- workers/restore_all.js
+++ workers/restore_all.js
@@ -35,19 +35,29 @@ spawn(function*(){
     })
   }
 
-  var result = yield query(`select id from ${TABLE} where s_digital is null and itunes is null limit 10`);
+  var result = yield query(`select id from ${TABLE} where s_digital is null and itunes is null`);
   log.info(`processing ${result.length} items`);
 
   for(var item of result){
-    var item_data = yield query(`select * from ${TABLE} where id = ${item.id}`);
-    var restored_data = yield [
-      yield itunes_restorer(item_data[0]),
-      yield s_digital_restorer(item_data[0])
-    ];
+    try {
+      var item_data = yield query(`select * from ${TABLE} where id = ${item.id}`);
+      var restored_data = yield [
+        yield itunes_restorer(item_data[0]),
+        yield s_digital_restorer(item_data[0])
+      ];
 
-    var restored_data = restored_data;
-    console.log(item_data[0].title);
-    console.log(restored_data);
+      var itunes_data = restored_data[0];
+      var s_digital_data = restored_data[1];
+      var values = [
+        `itunes = '${JSON.stringify(itunes_data)}'`,
+        `s_digital = '${JSON.stringify(s_digital_data)}'`
+      ].join(', ');
+      var query_text = `UPDATE ${TABLE} set ${values} WHERE id = ${item.id}`;
+      yield query(query_text);
+      log.info(`item #${item.id} updated`);
+    } catch(e) {
+      log.info(`could not process item #${item.id}`);
+    }
   }
 
   process.exit(0);
