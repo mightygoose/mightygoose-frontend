ce45c4226c6da273458c1340aed8a1f8eee8c2ce
Alexander Zhukov <al.zhukoff@gmail.com>
2016-06-29 23:19:15 +0200

    add preloader for search posts controller

--- assets/components/search_posts/search_posts.js
+++ assets/components/search_posts/search_posts.js
@@ -6,35 +6,51 @@ const Delegate = require('dom-delegate');
 
 
 class SearchPostsController extends BaseController {
+  render_first(append){
+    var [first, ...others] = this.posts;
+    if(!first){
+      return;
+    }
+    this.posts = others;
+    (this.preloader || (
+      this.preloader = this.childComponents.querySelector('mighty-preloader')
+    )).show();
+    fetch(`/api/post/${first}`)
+    .then((response) => response.json())
+    .then((posts) => {
+      this.childComponents.querySelector('posts-controller').render(posts, append);
+      this.preloader.hide();
+    });
+  }
   create() {
     console.log('search posts ctrl');
+
+    this.posts = [];
+
     this.html(template);
     this.addEventListener('mg-autocomplete-item-selected', (event) => {
       var {eventData: data} = event;
       if(data.type !== 'tags_suggestion'){
-        fetch(`/api/post/${data.id}`)
-        .then((response) => response.json())
-        .then((posts) => {
-          this.childComponents.querySelector('posts-controller').render(posts);
-        });
+        this.posts = [data.id];
+        this.render_first()
       } else {
         fetch(`/api/search/tag?q=${data.autocomplete_value}`)
         .then((response) => response.json())
         .then((ids) => {
-          //showcase
-          fetch(`/api/post/${ids[0]}`)
-          .then((response) => response.json())
-          .then((posts) => {
-            this.childComponents.querySelector('posts-controller').render(posts);
-          });
+          this.posts = ids;
+          this.render_first();
         });
       }
     });
 
+    window.addEventListener('scroll', (event) => {
+      if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+        this.render_first(true);
+      }
+    });
+
   }
-  attach(){
-    //debugger;
-  }
+  attach(){}
   detach(){}
   attributeChange(name, previousValue, value){}
 }
