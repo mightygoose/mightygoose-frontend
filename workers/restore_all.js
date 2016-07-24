6efa0dbc4e708093ade04801f5873b93777cb698
Alexander Zhukov <al.zhukoff@gmail.com>
2016-07-24 14:39:03 +0200

    refactor itunes restorer

--- workers/restore_all.js
+++ workers/restore_all.js
@@ -4,10 +4,10 @@ const massive = require("massive");
 const log = require('log-colors');
 const spawn = require('lib/spawn');
 
-const itunes_restorer = require('lib/restorers/itunes');
+const itunes_album_restorer = new (require('lib/restorers/itunes')).AlbumRestorer();
+const deezer_album_restorer = new (require('lib/restorers/deezer')).AlbumRestorer();
+
 const s_digital_restorer = require('lib/restorers/7digital');
-const DeezerRestorer = new require('lib/restorers/deezer');
-const deezer_album_restorer = new DeezerRestorer.AlbumRestorer();
 
 const DB_HOST = process.env['DB_HOST'];
 const DB_PORT = process.env['DB_PORT'];
@@ -47,24 +47,25 @@ spawn(function*(){
   }
 
   //var result = yield query(`select id from ${TABLE} where s_digital is null and itunes is null limit ${LIMIT}`);
-  var result = yield query(`select id from ${TABLE} where deezer is null limit ${LIMIT}`);
+  //var result = yield query(`select id from ${TABLE} where deezer is null limit ${LIMIT}`);
+  var result = yield query(`select id from ${TABLE} where deezer is null and itunes is null limit ${LIMIT}`);
   log.info(`processing ${result.length} items`);
 
   for(var item of result){
     try {
       var item_data = yield query(`select * from ${TABLE} where id = ${item.id}`);
       var restored_data = yield [
-        //yield itunes_restorer(item_data[0]),
+        yield itunes_album_restorer.restore(item_data[0]),
         yield deezer_album_restorer.restore(item_data[0])
         //yield s_digital_restorer(item_data[0])
       ];
 
-      //var itunes_data = restored_data[0];
-      var deezer_data = restored_data[0];
+      var itunes_data = restored_data[0];
+      var deezer_data = restored_data[1];
       //console.log(deezer_data);
       //var s_digital_data = restored_data[1];
       var values = [
-        //`itunes = '${JSON.stringify(itunes_data).replace(/'/ig, "''")}'`,
+        `itunes = '${JSON.stringify(itunes_data).replace(/'/ig, "''")}'`,
         `deezer = '${JSON.stringify(deezer_data).replace(/'/ig, "''")}'`,
         //`s_digital = '${JSON.stringify(s_digital_data).replace(/'/ig, "''")}'`
       ].join(', ');
