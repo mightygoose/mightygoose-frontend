8adb5cb5afab9b9d66a66f35dea764156c5a8f9a
Alexander Zhukov <al.zhukoff@gmail.com>
2016-10-09 13:44:40 +0200

    refactor(items processor): items processor as a class

--- workers/items_processor.js
+++ workers/items_processor.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const pry = require('pryjs');
 
 const jackrabbit = require('jackrabbit');
@@ -23,13 +25,35 @@ const DB_NAME = process.env['DB_NAME'];
 const RABBITMQ_URL = process.env['RABBITMQ_URL'];
 const RABBITMQ_CHANNEL = process.env['RABBITMQ_CHANNEL'];
 
-var db;
 
-spawn(function*(){
+class ItemsProcessor {
 
-  var connections = yield Promise.all([
-    //DB
-    new Promise((resolve) => {
+  constructor(){
+    var self = this;
+
+    self.postprocess = postprocess;
+
+    spawn(function*(){
+
+      var connections = yield [
+        ItemsProcessor.connect_to_db(),
+        ItemsProcessor.connect_to_queue()
+      ];
+
+      self.db = connections[0];
+      self.queue = connections[1];
+
+      self.queue
+      .default()
+      .queue({ name: RABBITMQ_CHANNEL })
+      .consume(self.process_item.bind(self), { noAck: true });
+    });
+
+    return this;
+  }
+
+  static connect_to_db(){
+    return new Promise((resolve) => {
       log.info('connecting to db');
       massive.connect({
         connectionString: `postgres://${DB_USER}:${DB_PASSWD}@${DB_HOST}/${DB_NAME}`
@@ -37,9 +61,11 @@ spawn(function*(){
         log.info('connected to db');
         resolve(_db);
       });
-    }),
-    //Q
-    new Promise((resolve) => {
+    });
+  }
+
+  static connect_to_queue(){
+    return new Promise((resolve) => {
       log.info('connecting to queue');
       var queue = jackrabbit(RABBITMQ_URL)
       .on('connected', function() {
@@ -53,132 +79,10 @@ spawn(function*(){
         log.info('disconnected from queue. exiting.');
         process.exit(0);
       });
-    }),
-  ]);
-
-  db = connections[0];
-  var queue = connections[1];
-
-  queue
-  .default()
-  .queue({ name: RABBITMQ_CHANNEL })
-  .consume(process_item, { noAck: true });
-});
-
-function process_item(item) {
-  spawn(function*(){
-
-    log.info(`got item #${item.sh_key}`);
-    var processed_item = Object.assign({}, item);
-
-    Object.assign(processed_item, {
-      discogs_data: yield discogs_album_restorer.restore(processed_item)
-    });
-
-    /* think of something better here */
-    Object.assign(processed_item, {
-      original_title: processed_item.title,
-    }, !processed_item.discogs_data ? {} : {
-      title: processed_item.discogs_data.title
-    });
-    /* / */
-
-    Object.assign(processed_item, {
-      restorers_data: yield {
-        itunes: itunes_album_restorer.restore(processed_item),
-        deezer: deezer_album_restorer.restore(processed_item)
-      }
-    });
-
-    Object.assign(processed_item, {
-      badges: generate_badges(processed_item)
-    });
-
-    Object.assign(processed_item, {
-      status: generate_status(processed_item)
-    });
-
-    Object.assign(processed_item, {
-      item_table: generate_table_name(processed_item)
-    });
-
-    Object.assign(processed_item, {
-      merged_tags: _.uniq(
-        JSON.parse(processed_item.tags).concat((processed_item.discogs_data || {genre: []}).genre)
-                                       .concat((processed_item.discogs_data || {style: []}).style)
-      )
-    });
-
-
-    var query_string = generate_query_string(processed_item);
-
-    if(process.env['NODE_ENV'] !== 'production'){
-      log.info(query_string);
-    } else {
-      var query_result = yield new Promise((resolve) => {
-        db.run(query_string, (err, items) => {
-          if(err){ log.error(err); }
-          log.info(`item added to table ${processed_item.item_table}`);
-          resolve(items);
-        });
-      });
-    }
-
-    postprocess(Object.assign({}, processed_item));
-
-  }).catch(e => log.error(e));
-}
-
-function generate_badges(item){
-  var badges = JSON.parse(item['badges']);
-  var discogs_data = item.discogs_data;
-  if(!discogs_data){
-    badges.push('discogs-no-results');
-  } else {
-    if(discogs_data.similarity === 1){
-      badges.push('discogs-title-exact-match');
-    } else {
-      var prepared_item_title = _.trim(
-        item['title']
-        .replace(/\(\d{4}\)/ig, '')
-        .replace(/\[\d{4}\]/ig, '')
-        .replace(/\d{0,4}kbps/ig, '')
-        .replace(/\(\)/ig, '')
-        .replace(/\[\]/ig, '')
-        .replace(/(\/.*)/ig, '')
-        .replace(/[^0-9a-zA-Z ]+/ig, '')
-        .toLowerCase());
-
-      var prepared_discogs_title = discogs_data['title'].replace(/[^0-9a-zA-Z ]+/ig, '').toLowerCase();
-      if(prepared_item_title === prepared_discogs_title){
-        badges.push('discogs-title-match-after-clean');
-      } else {
-        badges.push('discogs-title-doesnt-match');
-      }
-    }
+    })
   }
-  return badges;
-}
 
-function generate_status(item){
-  if(~item.badges.indexOf('discogs-no-results') || item.discogs_data.similarity <= 0.5){
-    return "bad";
-  }
-  return "good";
-}
-
-function generate_table_name(item){
-  switch(true){
-    case item.status === 'good':
-    case item.restorers_data.itunes && item.restorers_data.itunes.similarity === 1:
-    case item.restorers_data.deezer && item.restorers_data.deezer.similarity === 1:
-      return 'items';
-    default:
-      return 'bad_items';
-  }
-}
-
-function generate_query_string(item){
+  static generate_query_string(item){
 
     var prepared = _.pick(Object.assign({}, item, {
       discogs: _.pick(item.discogs_data, [
@@ -211,7 +115,140 @@ function generate_query_string(item){
       INSERT INTO ${item.item_table} (${data.fields.join(', ')})
       VALUES (${data.values.join(', ')});
     `;
+  }
+
+  static generate_badges(item){
+    var badges = JSON.parse(item['badges']);
+    var discogs_data = item.discogs_data;
+    if(!discogs_data){
+      badges.push('discogs-no-results');
+    } else {
+      if(discogs_data.similarity === 1){
+        badges.push('discogs-title-exact-match');
+      } else {
+        var prepared_item_title = _.trim(
+          item['title']
+          .replace(/\(\d{4}\)/ig, '')
+          .replace(/\[\d{4}\]/ig, '')
+          .replace(/\d{0,4}kbps/ig, '')
+          .replace(/\(\)/ig, '')
+          .replace(/\[\]/ig, '')
+          .replace(/(\/.*)/ig, '')
+          .replace(/[^0-9a-zA-Z ]+/ig, '')
+          .toLowerCase());
+
+        var prepared_discogs_title = discogs_data['title'].replace(/[^0-9a-zA-Z ]+/ig, '').toLowerCase();
+        if(prepared_item_title === prepared_discogs_title){
+          badges.push('discogs-title-match-after-clean');
+        } else {
+          badges.push('discogs-title-doesnt-match');
+        }
+      }
+    }
+    return badges;
+  }
+
+  static generate_status(item){
+    if(~item.badges.indexOf('discogs-no-results') || item.discogs_data.similarity <= 0.5){
+      return "bad";
+    }
+    return "good";
+  }
+
+  static generate_table_name(item){
+    switch(true){
+      case item.status === 'good':
+      case item.restorers_data.itunes && item.restorers_data.itunes.similarity === 1:
+      case item.restorers_data.deezer && item.restorers_data.deezer.similarity === 1:
+        return 'items';
+      default:
+        return 'bad_items';
+    }
+  }
+
+  static process_data(item){
+    return spawn(function*(){
+
+      log.info(`got item #${item.sh_key}`);
+      var processed_item = Object.assign({}, item);
+
+      Object.assign(processed_item, {
+        discogs_data: yield discogs_album_restorer.restore(processed_item)
+      });
+
+      /* think of something better here */
+      Object.assign(processed_item, {
+        original_title: processed_item.title,
+      }, !processed_item.discogs_data ? {} : {
+        title: processed_item.discogs_data.title
+      });
+      /* / */
+
+      Object.assign(processed_item, {
+        restorers_data: yield {
+          itunes: itunes_album_restorer.restore(processed_item),
+          deezer: deezer_album_restorer.restore(processed_item)
+        }
+      });
+
+      Object.assign(processed_item, {
+        badges: ItemsProcessor.generate_badges(processed_item)
+      });
+
+      Object.assign(processed_item, {
+        status: ItemsProcessor.generate_status(processed_item)
+      });
+
+      Object.assign(processed_item, {
+        item_table: ItemsProcessor.generate_table_name(processed_item)
+      });
+
+      Object.assign(processed_item, {
+        merged_tags: _.uniq(
+          JSON.parse(processed_item.tags).concat((processed_item.discogs_data || {genre: []}).genre)
+                                         .concat((processed_item.discogs_data || {style: []}).style)
+        )
+      });
+
+      return processed_item;
+
+    }).catch(e => log.error(`error while processing item. ${e}`));
+  }
+
+  process_item(item) {
+    return ItemsProcessor.process_data(item).then((processed_item) => {
+      var query_string = ItemsProcessor.generate_query_string(processed_item);
+
+      if(process.env['NODE_ENV'] !== 'production'){
+        log.info(query_string);
+      } else {
+        new Promise((resolve) => {
+          this.db.run(query_string, (err, items) => {
+            if(err){ log.error(err); }
+            log.info(`item added to table ${processed_item.item_table}`);
+            resolve(items);
+          });
+        });
+      }
+
+      //should be event here
+      this.postprocess(Object.assign({}, processed_item));
+    }).catch(e => log.error(e))
+  }
 }
+
+
+//run worker
+if(process.env['NODE_ENV'] !== 'test'){
+  new ItemsProcessor();
+}
+
+
+module.exports = ItemsProcessor;
+
+
+
+
 
 
 
