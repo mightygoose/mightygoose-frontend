a4365997485e87f57e6668c74a78fa688c96371d
Alexander Zhukov <a.zhukov@holidaypirates.com>
2016-12-08 16:33:28 +0100

    more tests

--- ascesis/test/router_spec.js
+++ ascesis/test/router_spec.js
@@ -5,10 +5,30 @@ window.history.replaceState(null, null, '/');
 
 describe("Base functionality", () => {
   const router = new Router();
+
+
   it('returns root correctly', () => {
     assert.equal(router.root, '');
   });
+
+  it('matches root correctly', () => {
+    assert.equal(router.root_matches, true);
+  });
+
+  it('returns query string correctly', () => {
+    assert.equal(router.qs, false);
+  });
+
   it('returns query params correctly', () => {
     assert.deepEqual(router.params, {});
   });
+
+  it('returns absolute path correctly', () => {
+    assert.equal(router.global_path, '/');
+  });
+
+  it('returns relative path correctly', () => {
+    assert.equal(router.path, '/');
+  });
+
 });
