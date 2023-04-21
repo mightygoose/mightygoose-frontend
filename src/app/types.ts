46214f0beb562c47de58fecfb2c1e4c3fd41a9b1
Aleksandr Zhukov <aleksandrzhukov@00008809s-MacBook-Pro.local>
2023-04-21 20:50:33 +0200

    fix type

--- src/app/types.ts
+++ src/app/types.ts
@@ -17,7 +17,7 @@ export interface Release {
   deezer?: Deezer;
   created_at: string;
   spotify?: Spotify;
-  similar_releases?: Array<Release>
+  similar_releases?: Array<Release>;
 }
 
 export interface Discogs {
